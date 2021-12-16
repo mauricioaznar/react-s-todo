4ffcf313f78db1506d9a1ac0986c15fc449e82ab
mauricioaznar <mauricioaznar94@gmail.com>
2021-12-16 10:46:37 -0600

    react springasdfa

--- src/templates/ui-engineering/responsiveness/views/ResponsivenessSolution.tsx
+++ src/templates/ui-engineering/responsiveness/views/ResponsivenessSolution.tsx
@@ -1,5 +1,15 @@
 import React from 'react'
+import {Box, Typography} from "@mui/material";
 
 export default function ResponsivenessSolution () {
-    return null
+    return (
+        <Box>
+            <Typography
+                variant={'h5'}
+            >
+
+                The ultimate solution
+            </Typography>
+        </Box>
+    )
 }
