7da2d510c2938e9087c96fe0c6334615998eed5b
mauricioaznar <mauricioaznar94@gmail.com>
2022-01-03 11:05:05 -0600

    author

--- src/templates/ui-engineering/responsiveness/views/ResponsivenessProblems.tsx
+++ src/templates/ui-engineering/responsiveness/views/ResponsivenessProblems.tsx
@@ -19,23 +19,18 @@ export default function ResponsivenessProblems() {
     },
     {
       problem: `Skipping frames feels “janky.`,
-      solutions: [],
       example: <JankyAnimation />,
     },
     {
       problem: `Indicators that cause the page layout to “jump” or that go through several loading “stages”
              can make the action feel longer than it was.`,
-      solutions: [],
       example: <UiBlocker />,
     },
     {
-      problem: `Handling an interaction within 20ms at the cost of dropping an
-             animation frame can feel slower than handling it within 30ms and no dropped frames.`,
-      solutions: [],
-    },
-    {
       problem: `How do we keep our apps responsive to different kinds of inputs?.`,
-      solutions: [],
+      answers: [
+        "If the action takes longer than 100ms, we display a loading state.",
+      ],
     },
   ];
 
