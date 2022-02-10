14547dd551f5e67163a7e85d1e80dbe23401a5b5
mauricioaznar <mauricioaznar94@gmail.com>
2022-02-10 12:46:40 -0600

    big refactor

--- src/helpers/sleep.ts
+++ src/helpers/sleep.ts
@@ -1,6 +1,5 @@
 export default function sleep(time: number) {
   return new Promise((resolve) => {
-    // eslint-disable-next-line no-undef
     setTimeout(() => {
       resolve(true);
     }, time);
